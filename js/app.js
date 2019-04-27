9d9741c1d66abac50c58efecf854f1dbf57fc6f9
Liliya Jeromin <liliyajeromin@Liliyas-Air.fritz.box>
2019-04-27 11:29:10 +0200

    everything but reload and timer

--- js/app.js
+++ js/app.js
@@ -3,7 +3,12 @@
  */
 
 const allCards = document.querySelectorAll('.card');
-
+let openedCards = [];
+ let counter = 0;
+ const movesCounter = document.querySelector('.moves');
+ const stars = document.querySelector('.stars');
+ const star = stars.firstElementChild;
+ let allStars = document.querySelectorAll('.fa-star');
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -12,42 +17,10 @@ const allCards = document.querySelectorAll('.card');
  */
 
  
-const allCardsArr = Array.from(allCards);
-const shuffledAllCards = shuffle(allCardsArr);
-
-for(let i = 0; i<16; i++){
-    //get the class to remove
-    var oldClass = allCards[i].querySelector('i').classList[1];
-
-    //get the class to add
-    var newClass = shuffledAllCards[i].querySelector('i').classList[1];
-
-    //just for debugging
-/* console.log('This is the element ' + i + ' of allCards ' + oldClass + ' and its gonna to be replaced with ' + newClass); */
-
-    //get the place where to change
-    var itemToChange = allCards[i].querySelector('i');
-
-    //change classes
-    itemToChange.classList.remove(oldClass);
-    itemToChange.classList.add(newClass);
-}
 
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+makeNewDeck();
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
 
 
 /*
@@ -61,12 +34,7 @@ function shuffle(array) {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
- let openedCards = [];
- let counter = 0;
- const movesCounter = document.querySelector('.moves');
- const stars = document.querySelector('.stars');
- 
-
+//action if a card is clicked
  allCards.forEach(function(card){
     card.addEventListener('click', function(){
 
@@ -77,7 +45,7 @@ function shuffle(array) {
     if(timeToRemoveStar()){
         stars.removeChild(stars.firstElementChild);
     }
-    
+
     //debugging
     console.log('moves ' + counter);
 
@@ -85,8 +53,8 @@ function shuffle(array) {
     card.classList.add('open', 'show');
 
     //debugging
-    console.log(openedCards);
-    console.log(openedCards.length);
+   // console.log(openedCards);
+   // console.log(openedCards.length);
     
     if (openedCards.length == 2){
          //check if they match
@@ -109,6 +77,83 @@ function shuffle(array) {
 });  
 });
 
+//reload
+
+const reload = document.querySelector('.restart');
+
+
+reload.addEventListener('click', function(){
+    
+    //all cards have only one class .card
+    makeAllCardsClose();
+    
+    //schuffle cards
+    makeNewDeck();
+
+    //set 0 moves 
+    counter=0;
+    movesCounter.innerHTML = counter;
+    
+    //set three stars
+    setNumberOfStars();
+
+    //set timer to 0
+    
+});
+
+//handy methods
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+//making new deck
+function makeNewDeck(){
+    const allCardsArr = Array.from(allCards);
+    const shuffledAllCards = shuffle(allCardsArr);
+
+    for(let i = 0; i<16; i++){
+        //get the class to remove
+        var oldClass = allCards[i].querySelector('i').classList[1];
+    
+        //get the class to add
+        var newClass = shuffledAllCards[i].querySelector('i').classList[1];
+    
+        //just for debugging
+    /* console.log('This is the element ' + i + ' of allCards ' + oldClass + ' and its gonna to be replaced with ' + newClass); */
+    
+        //get the place where to change
+        var itemToChange = allCards[i].querySelector('i');
+    
+        //change classes
+        itemToChange.classList.remove(oldClass);
+        itemToChange.classList.add(newClass);
+    }
+}
+
+function setNumberOfStars(){
+    if(allStars.length < 3){
+        console.log('fire');
+    }
+    
+}
+
+function makeAllCardsClose(){
+    allCards.forEach(function(card){
+    card.classList.remove('match', 'open','show');
+   });
+}
 
 function cardsMatch(array){
     return (openedCards[0].querySelector('i').classList[1] === openedCards[1].querySelector('i').classList[1]);
